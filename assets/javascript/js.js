29b2a8052abe0af287561cded28983a8170d53be
Non Dewman <Dnewman1934@yahoo.com>
2019-03-31 14:32:04 -0500

    lil scrip lil call lil html

---
+++ assets/javascript/js.js
@@ -0,0 +1,12 @@
+const apiKey = DrUotxLFBoXXpNInEw9qsNSqiG21xIaI;
+let searchQuery = prompt('enter search query: ');
+let Limit = prompt('How many gifs would you like?');
+let rating = prompt('Would you like to filter by rating?');
+const QueryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&limit=${Limit}&offset=0&rating=${rating}&lang=en`;
+$.ajax({
+    url: QueryUrl,
+    methodf: "GET"
+}).then(function (response) {
+    console.log(response);
+}
+);
