8d8b4b0708c981b30d1928732c882a694275ed5d
Non Dewman <Dnewman1934@yahoo.com>
2019-03-31 15:05:35 -0500

    mor scrip

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,8 +1,25 @@
 const apiKey = 'DrUotxLFBoXXpNInEw9qsNSqiG21xIaI';
-let searchQuery = prompt('enter search query: ');
+let breedArray = ["German Shepherd", "Pitbull", "Collie", "Doberman", "King Charles Spaniel", "corgi"]
+let searchQuery = breedArray
 let Limit = prompt('How many gifs would you like?');
-let rating = prompt('Would you like to filter by rating?');
-const QueryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&limit=${Limit}&offset=0&rating=${rating}&lang=en`;
+const QueryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&limit=${Limit}&offset=0&lang=en`;
+function renderButtons() {
+    for (i = 0; i < breedArray.length; i++) {
+        let newButton = $('</div>');
+        newButton.addClass('breed');
+        newButton.attr('data-name', breedArray[i]);
+        newButton.text(breedArray[i]);
+        $('#buttonsDiv').append(newButton);
+    }
+}
+$('#submitButton').on('click', function (event) {
+    event.preventDefault();
+    let breed = ('#breedInput').val().trim(); //make a breedinput
+    breedArray.push(breed);
+    renderButtons();
+}
+)
+renderButtons();
 $.ajax({
     url: QueryUrl,
     methodf: "GET"
@@ -10,4 +27,4 @@ $.ajax({
     console.log(response);
 }
 );
-let breedArray = ["German Shepherd", "Pitbull", "Collie", "Doberman", "King Charles Spaniel", "corgi"]
+console.log(breedArray)
