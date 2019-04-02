1ed0d8d41bdbc63f94084843cfda0509b64e2161
Non Dewman <Dnewman1934@yahoo.com>
2019-04-02 15:50:45 -0500

    button add function breaks dogbutton onclick :C

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,32 +1,48 @@
 const apiKey = 'DrUotxLFBoXXpNInEw9qsNSqiG21xIaI';
 let breedArray = ["German Shepherd", "Schnauzer", "Pitbull"]
 let searchQuery = breedArray
-const QueryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&limit=8&offset=0&lang=en`;
+
 function renderButtons() {
     $("#buttonsDiv").empty();
 
     for (i = 0; i < breedArray.length; i++) {
         let newButton = $('<button>');
-        newButton.addClass('breed');
+        newButton.addClass('dogButton');
         newButton.attr('data-name', breedArray[i]);
         newButton.text(breedArray[i]);
         $('#buttonsDiv').append(newButton);
     }
 }
+renderButtons();
+console.log(breedArray)
 $('#add-breed').on('click', function (event) {
     event.preventDefault();
-    var breed = $('#breed-input').val().trim();
-    breedArray.push(breed);
+    var breedAddition = $('#breed-input').val().trim();
+    breedArray.push(breedAddition);
     renderButtons();
 }
 );
-renderButtons();
-$.ajax({
-    url: QueryUrl,
-    methodf: "GET"
-}).then(function (response) {
-    console.log(response);
-}
-);
-console.log(breedArray)
 
+$('.dogButton').on('click', function (event) {
+    var breed = $(this).attr("data-name");
+    const QueryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${breed}&limit=8&offset=0&lang=en`;
+    $.ajax({
+        url: QueryUrl,
+        methodf: "GET"
+    }).then(function (response) {
+        var callBack = response.data;
+        console.log(response);
+        console.log(response.data);
+        for (var i = 0; i < callBack.length; i++) {
+            var newGif = $('<div>');
+            var rating = callBack[i].rating;
+            var ratingP = $('<p>').text('Rating: ' + rating);
+            var pupGif = $('<img>')
+            pupGif.attr('src', callBack[i].images.fixed_height.url);
+            newGif.append(ratingP);
+            newGif.append(pupGif);
+            $('#gifsDiv').prepend(newGif)
+        }
+    }
+    );
+})
